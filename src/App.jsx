d33ab8120638973a7d24ad6ca5ff824313faca18
landocle <86326695+landocle@users.noreply.github.com>
2024-12-31 07:37:04 -0500

    Setting up deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 
-import '@Styles/App.scss';
+import '@Styles/app.scss';
 
 import { Home } from '@Pages';
 import { GameFactory } from '@Elements';
