d4140a0954166d0c67f08e5b289bfbcb6735e953
landocle <86326695+landocle@users.noreply.github.com>
2024-12-30 22:11:15 -0500

    Removing analytics and footer information

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React, { Fragment } from 'react';
 import { KeyBoardManagar } from '@HOC';
 
-import { Instruction, Header, Game, Details } from '@Components';
-import { Container, Wave, GameFactoryConsumer } from '@Elements';
+import { Instruction, Header, Game } from '@Components';
+import { Container, GameFactoryConsumer } from '@Elements';
 
 import Waves from '@Image/waves.gif';
 
@@ -18,17 +18,6 @@ const Home = ({ eventType }) => {
               <Game eventType={eventType} {...values} {...methods} />
               <br />
               <Instruction />
-              <div>
-                <Wave className="waves" src={Waves} alt="" />
-              </div>
-              <br />
-              <Details
-                name={'Shubham Singh'}
-                githubURL={'https://github.com/imshubhamsingh'}
-                projectURL={'https://github.com/imshubhamsingh/15-puzzle'}
-                linkedinURL={'https://linkedin.com/in/imshubhamsingh97/'}
-                twitterURL={'https://twitter.com/imshubhamsingh_'}
-              />
             </Fragment>
           );
         }}
